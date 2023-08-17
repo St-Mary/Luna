7939fc6668ea88fae15ab356b0817f306483ffec
Noëlle <46852063+Noelle-Ai@users.noreply.github.com>
2023-08-17 22:19:19 +0200

    fix: Fix acknowledgement error

--- src/main/java/me/aikoo/StMary/commands/Menu.java
+++ src/main/java/me/aikoo/StMary/commands/Menu.java
@@ -132,7 +132,6 @@ public class Menu extends AbstractCommand {
                     "▬▬▬▬▬▬▬▬▬▬\n";
 
             event.editMessage(stringBuilder).queue();
-            event.deferEdit().queue();
         }
     }
 
